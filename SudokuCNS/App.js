f3f526d7e62395f15d4e49973173528462cbf17e
Jobin Mathew <jobinmatt@live.ca>
2020-01-31 14:20:27 -0500

    simple UI to get started

--- SudokuCNS/App.js
+++ SudokuCNS/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import { StyleSheet, Dimensions, Platform } from 'react-native';
+import { StyleSheet, Dimensions, Platform, StatusBar } from 'react-native';
 import { ApplicationProvider, Layout, Text, Button } from '@ui-kitten/components';
 import { mapping, light, dark } from '@eva-design/eva';
 import { RNCamera, } from 'react-native-camera';
 import * as Permissions from 'expo-permissions';
 import { PermissionsAndroid } from 'react-native';
 
+const GRANTED = 'granted';
+const DENIED = 'denied';
 var { windowHeight, windowWidth } = Dimensions.get('window');
+
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cameraPermissions: ''
+      cameraPermissions: GRANTED //TODO: setting to Granted to bypass screen for now
     };
-    this.getCameraPermissions();
+    // this.getCameraPermissions();
   }
 
   async getCameraPermissions() {
     if (Platform.OS = 'android') {
       try {
-        if (this.state.cameraPermissions !== 'granted') {
+        if (this.state.cameraPermissions !== GRANTED) {
           var granted = await PermissionsAndroid.request(
             PermissionsAndroid.PERMISSIONS.CAMERA, {
             title: 'This application needs access to the camera to take pictures of sudoku puzzles.',
@@ -29,7 +32,7 @@ export default class App extends React.Component {
           },
           );
           if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-            this.setState({ cameraPermissions: 'granted' });
+            this.setState({ cameraPermissions: GRANTED });
             console.log('You can use the camera');
           } else {
             console.log('Camera permission denied');
@@ -42,7 +45,7 @@ export default class App extends React.Component {
   }
 
   render() {
-    if (this.state.cameraPermissions !== 'granted') {
+    if (this.state.cameraPermissions !== GRANTED) {
       return (
         <ApplicationProvider mapping={mapping} theme={dark}>
           <Layout level='3' style={styles.container}>
@@ -53,12 +56,16 @@ export default class App extends React.Component {
     } else {
       return (
         <ApplicationProvider mapping={mapping} theme={dark}>
+          <Layout>
+            {Platform.OS === 'ios' && <StatusBar barStyle={'default'}/>}
+            {Platform.OS === 'android' && <Layout style={{ marginTop: StatusBar.currentHeight }} />}
+          </Layout>
           <Layout level='3' style={styles.container}>
-            <Layout level='1' style={[styles.cameraContainer, { width: windowWidth, height: windowWidth }]}>
-              <RNCamera></RNCamera>
+            <Layout level='1' style={styles.cameraContainer}>
+              {/* <RNCamera style={[styles.camera, { width: windowWidth, height: windowWidth }]}></RNCamera> */}
             </Layout>
             <Layout level='2' style={styles.buttonContainer}>
-              <Button>Capture</Button>
+              <Button style={styles.button}>Capture</Button>
             </Layout>
           </Layout>
         </ApplicationProvider>
@@ -76,16 +83,25 @@ const styles = StyleSheet.create({
   },
   cameraContainer: {
     flex: 1,
-    backgroundColor: 'pink',
+    margin: 8,
+    borderRadius: 10,
     alignItems: 'center',
     justifyContent: 'center',
   },
   camera: {
     flex: 1,
+    borderRadius: 10,
   },
   buttonContainer: {
-    flex: 1,
+    borderRadius: 10,
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    flex: 1,
+    padding: 8,
+    margin: 8,
+    borderRadius: 10,
   },
 });
